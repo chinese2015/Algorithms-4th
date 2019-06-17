4d04d3a6e70d150d62af89002619fca14ea176dc
chinese2015 <huaxia2002@gmail.com>
2019-06-17 19:02:27 +0800

    toBinaryString

--- src/com/huaxia/algorithms/chapter1_1/Practise1_1_9.java
+++ src/com/huaxia/algorithms/chapter1_1/Practise1_1_9.java
@@ -6,11 +6,16 @@ public class Practise1_1_9 {
 		int n = 1024;
 		//自带方法
 		//String s = Integer.toBinaryString(n);
+		String s =toBinaryString(n);
 		//输入1024,输出10000000000
 		System.out.println(s);
 		}
 	
 	public static String toBinaryString(int n){
-		
+		String s = "";
+		for(int j = n;j>0;j/=2){
+			s = (j%2)+s;
+		}
+		return s;
 	}
 }
