36f05c8c8daa293ea99d178782dd608b2978c23a
chinese2015 <huaxia2002@gmail.com>
2019-08-05 11:52:33 +0800

    EXT17 added

--- src/com/huaxia/algorithms/chapter1_4/Ext16and17.java
+++ src/com/huaxia/algorithms/chapter1_4/Ext16and17.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Ext16and17 {
 	
-	public static int[] getLeastDifferencePair (int[] nums){
+	public static void getLeastDifferencePair (int[] nums){
 		//归并排序时间成本为NlnN
 		Arrays.sort(nums);
 		int leastDifference = Math.abs(nums[0]-nums[1]);
@@ -16,9 +16,30 @@ public class Ext16and17 {
 				flag = i;
 			}
 		}
-		return {nums(flag),nums(flag+1)};
+		System.out.println("MinDiffer:"+nums[flag]+","+nums[flag+1]);
 		
 		
+		
+	}
+	public static void getMaxDifferencePair(int[] nums){
+		
+		//定义最小值和最大值后,直接遍历,若当前值比最小值小或最大值大,则更新最大值或最小值
+		int[] a =nums;
+		int min = a[0];
+		int max = a[1];
+		for(int i =0;i<nums.length;i++){
+			if(a[i]>max){
+				max=a[i];
+			}else if(a[i]<min){
+				min=a[i];
+			}
+		}
+		System.out.println("MaxDiff:"+min+","+max);
+	}
+	public static void main(String[] args) {
+		int[] arrs = {-2,3,55,46645,32223,1,244,9999};
+		getLeastDifferencePair(arrs);
+		getMaxDifferencePair(arrs);
 	}
 
 }
